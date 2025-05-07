9b2d203121b99efb44541d0374655be5c48f5952
Daniel Prado <daniel.vascomadagama2@gmail.com>
2025-05-07 08:38:18 -0300

    a

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0, 1, -10);
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            transform.position = player.position + offset;
+        }
+    }
+}
