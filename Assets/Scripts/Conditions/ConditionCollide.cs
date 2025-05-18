bb587f3f43976462c569e031dce5f96dbd1d330d
mmp052 <mateusmoreirammp052@gmail.com>
2025-05-18 02:22:36 -0300

    esse ta errado mas vai

--- Assets/Scripts/Conditions/ConditionCollide.cs
+++ Assets/Scripts/Conditions/ConditionCollide.cs
@@ -1,14 +1,4 @@
 using UnityEngine;
-<<<<<<< HEAD
-
-namespace FSM
-{
-    public class ConditionCollide: Condition
-    {
-        Enemy agent;
-
-        public ConditionCollide(Enemy ag) 
-=======
 using FSM;  // opcional, mas mantém a consistência
 
 namespace FSM
@@ -18,23 +8,14 @@ namespace FSM
         private Enemy agent;
 
         public ConditionCollide(Enemy ag)
->>>>>>> 07ec0b5fcdb43dd9f2f3417bf5dd1e1c2f8c276a
         {
             agent = ag;
         }
 
         public override bool Test()
         {
-<<<<<<< HEAD
-            return agent.flagEnterAttack;
-        }
-
-    }
-}
-=======
             // usa o nome da propriedade tal como está em Enemy.cs
             return agent.FlagEnterAttack;
         }
     }
 }
->>>>>>> 07ec0b5fcdb43dd9f2f3417bf5dd1e1c2f8c276a
